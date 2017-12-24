cd66c366c2c0fae77ae59e1ac12c84502916af52
Daniel Meltzer <meltzerdanielebay@gmail.com>
2017-12-24 20:41:42 +0200

    I hate git

---
+++ cmake-build-debug/CMakeFiles/FourInARowUniveristyProject.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/meltzer/Documents/FourInARowUniveristyProject/SPArrayList.c" "/Users/meltzer/Documents/FourInARowUniveristyProject/cmake-build-debug/CMakeFiles/FourInARowUniveristyProject.dir/SPArrayList.c.o"
+  "/Users/meltzer/Documents/FourInARowUniveristyProject/SPArrayListUnitTest.c" "/Users/meltzer/Documents/FourInARowUniveristyProject/cmake-build-debug/CMakeFiles/FourInARowUniveristyProject.dir/SPArrayListUnitTest.c.o"
+  "/Users/meltzer/Documents/FourInARowUniveristyProject/SPFIARGameUnitTest.c" "/Users/meltzer/Documents/FourInARowUniveristyProject/cmake-build-debug/CMakeFiles/FourInARowUniveristyProject.dir/SPFIARGameUnitTest.c.o"
+  "/Users/meltzer/Documents/FourInARowUniveristyProject/SPFIARParser.c" "/Users/meltzer/Documents/FourInARowUniveristyProject/cmake-build-debug/CMakeFiles/FourInARowUniveristyProject.dir/SPFIARParser.c.o"
+  "/Users/meltzer/Documents/FourInARowUniveristyProject/SPFIARParserUnitTest.c" "/Users/meltzer/Documents/FourInARowUniveristyProject/cmake-build-debug/CMakeFiles/FourInARowUniveristyProject.dir/SPFIARParserUnitTest.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/FourInARowUniveristyProject.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FourInARowUniveristyProject.dir/SPArrayList.c.o"
+  "CMakeFiles/FourInARowUniveristyProject.dir/SPArrayListUnitTest.c.o"
+  "CMakeFiles/FourInARowUniveristyProject.dir/SPFIARGameUnitTest.c.o"
+  "CMakeFiles/FourInARowUniveristyProject.dir/SPFIARParser.c.o"
+  "CMakeFiles/FourInARowUniveristyProject.dir/SPFIARParserUnitTest.c.o"
+  "FourInARowUniveristyProject.pdb"
+  "FourInARowUniveristyProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FourInARowUniveristyProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
